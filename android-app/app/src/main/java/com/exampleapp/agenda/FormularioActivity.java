523f37f6915895005346195e74517f3d70cac002
tiago.albuquerque <tiagoamp@>
2018-11-27 08:58:58 -0200

    First commit

---
+++ android-app/app/src/main/java/com/exampleapp/agenda/FormularioActivity.java
@@ -0,0 +1,60 @@
+package com.exampleapp.agenda;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.exampleapp.agenda.model.Aluno;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    private FormularioHelper helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+        helper = new FormularioHelper(this);
+
+        /*Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioActivity.this, "Aluno salvo!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });*/
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+                Aluno aluno = helper.pegaAluno();
+                Toast.makeText(FormularioActivity.this, "Aluno "+ aluno.getNome() + " salvo!", Toast.LENGTH_SHORT).show();
+
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
