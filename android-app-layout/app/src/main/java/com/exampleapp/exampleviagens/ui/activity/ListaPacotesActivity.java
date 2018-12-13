b64ab7bf6312299eeafe4b6cedbcdc4c335c07b7
tiago.albuquerque <tiagoamp@>
2018-12-13 09:30:20 -0200

    First commit of new Constraint Layout example app

---
+++ android-app-layout/app/src/main/java/com/exampleapp/exampleviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,17 @@
+package com.exampleapp.exampleviagens.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.exampleapp.exampleviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+
+
+    }
+}
