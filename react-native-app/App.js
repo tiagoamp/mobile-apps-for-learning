ec7022d926283b38410ca56db9902b4d5d7d92e9
tiago.albuquerque <tiagoamp@>
2019-01-31 08:31:37 -0200

    Apply StyleSheet

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -8,13 +8,11 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, Dimensions} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, Dimensions, ScrollView, FlatList} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 });
 
 const width = Dimensions.get('screen').width;
@@ -23,36 +21,49 @@ type Props = {};
 export default class App extends Component<Props> {
 
   render() {
-    const fotos = [ {id: 1, usuario: 'tiago'}, {id: 2, usuario: 'alberto'}, {id: 3, usuario: 'joao'} ];
+    const fotos = [ {id: 1, usuario: 'tiagoamp'}, {id: 2, usuario: 'ozzy'}, {id: 3, usuario: 'max'} ];
 
     return (
-      <View style={{marginTop: 20}}>
-        {fotos.map(foto =>
-            <View key={foto.id}>
-              <Text>{foto.usuario}</Text>
-              <Image source={require('./resources/img/reactnative.png')} style={{width:width, height:width}} />
-            </View>
-        )}
-      </View>
+      <FlatList style={styles.container} data={fotos} keyExtractor={item => item.id}
+          renderItem={ ({item}) => 
+              <View>
+                <View style={styles.cabecalho} >
+                  <Image source={require('./resources/img/reactnative.png')} style={styles.fotoDePerfil} />
+                  <Text>{item.usuario}</Text>
+                </View>
+                <Image source={require('./resources/img/reactnative.png')} style={styles.foto} />
+              </View>
+          }
+        />
+
+      // <ScrollView style={{marginTop: 20}}>
+      //   {fotos.map(foto =>
+      //       <View key={foto.id}>
+      //         <Text>{foto.usuario}</Text>
+      //         <Image source={require('./resources/img/reactnative.png')} style={{width:width, height:width}} />
+      //       </View>
+      //   )}
+      // </ScrollView>
     );
   }
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20
+  },
+  cabecalho: {
+    margin: 10, 
+    flexDirection: 'row', 
+    alignItems: 'center'
+  },
+  fotoDePerfil: {
+    marginRight: 10, 
+    borderRadius: 20, 
+    width:40, height:40
+  },
+  foto: {
+    width:width, 
+    height:width
+  }
+})
