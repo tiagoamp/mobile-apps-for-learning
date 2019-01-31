33b6691e662009198d1fecb37bcfe6b8b4c55409
tiago.albuquerque <tiagoamp@>
2019-01-31 08:13:52 -0200

    First app customizations

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -8,7 +8,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, Dimensions} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,35 +17,42 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const width = Dimensions.get('screen').width;
+
 type Props = {};
 export default class App extends Component<Props> {
+
   render() {
+    const fotos = [ {id: 1, usuario: 'tiago'}, {id: 2, usuario: 'alberto'}, {id: 3, usuario: 'joao'} ];
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Hello World  !!!</Text>
-        <Text style={styles.welcome}>Welcome to React Native, by tiagoamp!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{marginTop: 20}}>
+        {fotos.map(foto =>
+            <View key={foto.id}>
+              <Text>{foto.usuario}</Text>
+              <Image source={require('./resources/img/reactnative.png')} style={{width:width, height:width}} />
+            </View>
+        )}
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
